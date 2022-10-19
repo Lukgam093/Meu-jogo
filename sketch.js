fa55005767cba5ba7881897118470402e9c9aedc
Lukgam093 <104927364+Lukgam093@users.noreply.github.com>
2022-10-19 19:03:41 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 var caminho1,caminho2,caminho3,caminho4,caminho5,caminho6;
 var inimigos, inimigosGroup;
-var alvo;
+var alvo, score = 0, vidas = 3;
 var jogador, jogador2, jogador3;
 var bala,balasRoxa,balasAzul;
+var gamestate = 0;
+var ativaAmarelo = true;
 
 function setup(){
   createCanvas(windowWidth-50, windowHeight-50);
@@ -28,16 +30,48 @@ function setup(){
   jogador.shapeColor = "blue";
   jogador2 = createSprite(1200, 250, 40, 40);
   jogador2.shapeColor = "yellow";
+  jogador2.debug = true;
+  
   jogador3 = createSprite(1200, 400, 40, 40);
   jogador3.shapeColor = "purple";
   
+  //botão inicial
+  button = createButton("jogar");
+  button.position(width/2, height/2);
+  button.size(200,100);
+
+  //pagina inicial
+  titulo = createElement("h2");
+  titulo.position(width/2, height/2-150);
+  titulo.html("defesa de torre");
+
 }
 
 function draw(){
   background("green");
-  gerarInimigo();
-  //console.log(World.mouseY);
 
+    //estado inicial
+  if(gamestate === 0){
+    background("blue");
+    button.mouseClicked(()=>{
+      titulo.hide();
+      button.hide();
+      gamestate = 1;
+    })
+  }
+  
+   //estado jogar
+  if(gamestate === 1){
+   
+    //mostrando pontuação e vidas
+  textSize(30);
+  fill("yellow");
+  text("score:" + score, 50, 60);
+  text("vidas:" + vidas, 50, 25);
+
+  gerarInimigo();
+  
+  //movimentando os inimigos pelo caminho
   for(var i = 0;i<inimigosGroup.length;i = i+1){
     if(inimigosGroup[i].x === 100){
       inimigosGroup[i].setVelocity(0, 4);
@@ -49,28 +83,16 @@ function draw(){
 
     if(inimigosGroup[i].x === 580){
       inimigosGroup[i].setVelocity(0, -4);
-      //console.log(inimigosGroup[0].y);
-      
     }
     
     if(inimigosGroup[i].x === 580 && inimigosGroup[i].y === 124){
       inimigosGroup[i].setVelocity(4, 0);
-      //console.log("entrou");
     }
+
     if(inimigosGroup[i].x === 1004){
       inimigosGroup[i].setVelocity(0, 4);
-      //console.log(inimigosGroup[0].y);
     }
   }
-
-  //verificando colisão do inimigo com o alvo
-  if(inimigosGroup.isTouching(alvo)){
-  for(var i = 0;i<inimigosGroup.length;i = i+1){
-   if(inimigosGroup[i].isTouching(alvo)){
-    inimigosGroup[i].destroy();
-   } 
-  }
-} 
 
   //movimentando jogador com mouse
   if(mousePressedOver(jogador)){
@@ -101,6 +123,7 @@ function draw(){
       if(inimigosGroup[i].isTouching(balasRoxa)){
        inimigosGroup[i].destroy();
        balasRoxa.destroyEach();
+       score = score + 5;
       } 
   }
 }
@@ -134,8 +157,55 @@ if(balasAzul.isTouching(inimigosGroup)){
   }
 }
 
+  //arma do jogador amarelo
+  if(keyWentDown("E")){
+    if(ativaAmarelo === true){
+      jogador2.setCollider("rectangle", 0, 0, 250, 250);
+    if(inimigosGroup.isTouching(jogador2)){
+      for(var i = 0;i<inimigosGroup.length;i = i+1){
+        if(inimigosGroup[i].isTouching(jogador2)){
+         inimigosGroup[i].destroy();
+         score = score + 5;
+        } 
+    }
+    ativaAmarelo = false;
+    }
+  }
+  }
+  if(keyWentUp("E")){
+    jogador2.setCollider("rectangle", 0, 0, 40, 40);
+  }
+
+  //ativando amarelo apos 100 frames
+  if(frameCount%100 === 0){
+    ativaAmarelo = true
+  }
+
+  //verificando colisão do inimigo com o alvo
+  if(inimigosGroup.isTouching(alvo)){
+    for(var i = 0;i<inimigosGroup.length;i = i+1){
+     if(inimigosGroup[i].isTouching(alvo)){
+      inimigosGroup[i].destroy();
+      vidas = vidas - 1;
+     } 
+    }
+  } 
+
+  //condição para o fim de jogo
+  if(vidas === 0){
+    gamestate = 2;
+  }
 
   drawSprites();
+  }
+
+  //estado final
+  if(gamestate === 2){
+    background("red");
+    textSize(40);
+    fill("black");
+    text("Boa sorte na proxima vez!!!", width/2-200, height/2);
+  }
 }
 
 function gerarInimigo(){
@@ -154,6 +224,7 @@ inimigosGroup.add(inimigos);
 
   function gerarBalas(x,y,vx,vy,group){
     bala = createSprite(x, y, 10, 10);
+    bala.shapeColor = "orange";
     bala.velocityY = vy;
     bala.velocityX = vx;
     group.add(bala);
